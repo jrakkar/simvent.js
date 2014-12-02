cb7339bc685a549b14116ffb27b8262cd773cd2f
Nicolas Blais St-Laurent <line_allo@yahoo.ca>
2014-12-02 16:09:34 -0500

    Variables renaming wort bigined

--- simvent.js
+++ simvent.js
@@ -2,16 +2,34 @@ var sv = {}
 
 sv.log = function(lung, vent){
 	return {
+
+		// Lung variables
+		
+		time: vent.time,
+		Flung: lung.flow,
+		Palv: lung.Palv,
+		Vc: lung.Vc,
+		Vci: lung.Vci,
+		Vce: lung.Vce,
+		Pco2: lung.Pco2,
+		Sco2: lung.Sco2,
+		Vco2: lung.Vco2,
+
+		// Ventilator variables
+
+		Fip: lung.Fop,
+		Fop: lung.Fip
+		Pcirc: vent.Pcirc,
 	};
 }
 
 sv.SimpleLung = function(){
 
 	// Propriété statiques
-	this.echantillonnage = 0.001; // Secondes
-	this.compliance = 50.0 ;// ml/cmH2O
-	this.resistance = 5.0 ;// cmH2O/l/s
-	this.Vem = 0.1;
+	this.Tsamp = 0.001; // Secondes
+	this.Crs = 50.0 ;// ml/cmH2O
+	this.Raw = 5.0 ;// cmH2O/l/s
+	this.Vdaw = 0.1;
 	this.PiCO2 = 0.0;
 	this.PACO2 = 35.0;
 	this.Pente2 = 0.003;
@@ -22,23 +40,23 @@ sv.SimpleLung = function(){
 	this.fco2 = 0;
 	this.vc = 0.0;
 	this.palv = 0.0;
-	this.debit = 0.0;
+	this.flow = 0.0;
 	this.vcmax = 0;
 	this.veco2 = 0;
 	
 	this.appliquer_pression = function (pression, duree){
 
-		var temps = 0.0;
+		var time = 0.0;
 		var deltaVolume = 0.0;
 
-		while (temps < duree){
+		while (time < duree){
 
-			this.debit = (pression - this.palv) / this.resistance ; // l/s
-			deltaVolume = this.debit * this.echantillonnage; // l
+			this.flow = (pression - this.palv) / this.resistance ; // l/s
+			deltaVolume = this.flow * this.echantillonnage; // l
 			this.vc += deltaVolume; // l
 			this.palv = (1000 *this.vc)  / (this.compliance);
 
-			if (this.debit > 0){
+			if (this.flow > 0){
 				this.vcmax = this.vc;
 				this.pco2 = 0;
 				this.vce = 0;
@@ -52,7 +70,7 @@ sv.SimpleLung = function(){
 				this.veco2 += this.fco2 * (-deltaVolume);
 			}
 
-			temps += this.echantillonnage;
+			time += this.echantillonnage;
 		}
 	};
 
@@ -78,58 +96,58 @@ sv.PresureControler = function(){
 	this.Pinspi = 10;
 	this.PEP = 0;
 	this.Ti = 1;
-	this.temps = 0;
+	this.time = 0;
 	this.echantillonnage = 0.001;
 	this.nbcycles = 3;
 	
-	this.ventiler = function(poumon){
+	this.ventiler = function(lung){
 
 		var ventd = [];
 		var respd = [];
-		this.temps = 0.0;
+		this.time = 0.0;
 		for (c=0;c < this.nbcycles;c++){
-			var tdeb = this.temps;
+			var tdeb = this.time;
 
-			while(this.temps < (tdeb + this.Ti)){
-				poumon.appliquer_pression(this.Pinspi, this.echantillonnage)
+			while(this.time < (tdeb + this.Ti)){
+				lung.appliquer_pression(this.Pinspi, this.echantillonnage)
 
 				ventd.push({
-					temps: this.temps,
-					debit: poumon.debit,
-					palv: poumon.palv,
+					temps: this.time,
+					debit: lung.flow,
+					palv: lung.palv,
 					pcirc: this.Pinspi,
-					vc: poumon.vc,
-					vce: poumon.vce,
-					pco2: poumon.pco2,
-					fco2: poumon.fco2,
-					vco2: poumon.veco2
+					vc: lung.vc,
+					vce: lung.vce,
+					pco2: lung.pco2,
+					fco2: lung.fco2,
+					vco2: lung.veco2
 				});
 
-				this.temps += this.echantillonnage;	
+				this.time += this.echantillonnage;	
 			}
 
-			while(this.temps < (tdeb + this.Ti *3)){
-				poumon.appliquer_pression(this.PEP, this.echantillonnage)
+			while(this.time < (tdeb + this.Ti *3)){
+				lung.appliquer_pression(this.PEP, this.echantillonnage)
 				ventd.push({
-					temps: this.temps,
-					debit: poumon.debit,
-					palv: poumon.palv,
+					temps: this.time,
+					debit: lung.flow,
+					palv: lung.palv,
 					pcirc: this.PEP,
-					vc: poumon.vc,
-					vce: poumon.vce,
-					pco2: poumon.pco2,
-					fco2: poumon.fco2,
-					vco2: poumon.veco2
+					vc: lung.vc,
+					vce: lung.vce,
+					pco2: lung.pco2,
+					fco2: lung.fco2,
+					vco2: lung.veco2
 				});
-				this.temps += this.echantillonnage;	
+				this.time += this.echantillonnage;	
 			}
-			var pmeco2 = ((760-47) * poumon.veco2/poumon.vce);
+			var pmeco2 = ((760-47) * lung.veco2/lung.vce);
 			respd.push({
 				pmeco2:pmeco2,
-				petco2:poumon.pco2,
-				pAco2: poumon.PACO2,
-				fowler: poumon.Vem/poumon.vce,
-				bohr: (poumon.PACO2 - pmeco2)/poumon.PACO2,
+				petco2:lung.pco2,
+				pAco2: lung.PACO2,
+				fowler: lung.Vem/lung.vce,
+				bohr: (lung.PACO2 - pmeco2)/lung.PACO2,
 			});
 		}
 
@@ -145,6 +163,12 @@ sv.VDR = {
 	Tec: 1, // Convective expiratory time
 	Tip: 1, // Percussive expiratory time
 	Tep: 1, // Percussive expiratory time
+
+	/* Presure at the airway openning does not usualy fall
+	 * immediatly at zero during expiratory phase. This suggest
+	 * a small resistance of the expiratory circuit.
+	 */
+	Rexp: 1 // cmH2O/l/s. To be adjusted based on the visual aspect of the curve.
 };
 
 sv.VDR.log = function(lung){
