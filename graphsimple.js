7b67d709f2e5b21ef460721a7a6462130d0f6348
ProgRT <line_allo@yahoo.ca>
2017-01-15 07:18:46 -0500

    graphsimple: plagey created

--- graphsimple.js
+++ graphsimple.js
@@ -291,20 +291,27 @@ gs.graph = class {
 	plagey (min, max, id){
 		var pad = this.padPlage;
 		var plage = {};
+
 		plage.ligne = this.svg.append("line")
-			.attr("x1", this.echellex(min) + pad)
-			.attr("x2", this.echellex(min)+ pad + 6)
-			//.attr("y1", this.height - (this.margeB - 15))
-			//.attr("y2", this.height - (this.margeB - 15))
-			.attr("y1", this.height - this.margeB/1.5)
-			.attr("y2", this.height - this.margeB/1.5)
-			.attr("class", "axe")
+			.attr("y1", this.echelley(min) - pad)
+			.attr("y2", this.echelley(min)- pad - 6)
+			.attr("class", "help")
 			.attr("style", "marker-start: url(#fleches);marker-end: url(#flechep);");
-			plage.ligne.transition().duration(this.durAnim).attr("x2", this.echellex(max)- pad);
+
+		if(this.annotateOnRight == true){
+			plage.ligne
+				.attr("x1", this.width - this.margeD/2)
+				.attr("x2", this.width - this.margeD/2);
+		}
+
+		plage.ligne.transition()
+			.duration(this.durAnim)
+			.attr("y2", this.echelley(max) + pad);
 
 		plage.texte = this.svg.append("text")
-			.attr("x", this.echellex(min + (max - min)/2))
-			.attr("y", this.height - this.margeB/2 + 20)
+			.attr("class", "help")
+			.attr("y", this.echelley(min + (max - min)/2))
+			.attr("x", this.width - this.margeD/2)
 			.attr("text-anchor", "middle")
 			.text(id)
 			.attr("opacity", 0);
