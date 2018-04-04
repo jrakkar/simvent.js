6a3426f77c9549eebc555cb7867b1dc7c04f2d5e
* <nicolas.blais.stlaurent@gmail.com>
2018-04-04 13:01:15 -0400

    fixed classList bug

--- src/ventyaml.js
+++ src/ventyaml.js
@@ -1,9 +1,11 @@
 class ventyaml {
 	constructor(sourceNode) {
 		this.clsList = sourceNode.classList;
+		/***
 		for(var cls in sourceNode.classList.keys()){
 			console.log(cls);
 		}
+		***/
 
 		if (! YAML){throw "ventyaml: YAML library not loaded."}
 
