f933fa985a99ae775a7079eb4d2a4ce0a004a641
ProgRT <nicolas.blais.stlaurent@gmail.com>
2018-09-04 13:39:44 -0400

    simvent.js updated from gh-pages

--- src/simvent.js
+++ src/simvent.js
@@ -352,9 +352,9 @@ sv.SptLung = class SptLung extends sv.Lung{
 	get Pmus(){
 		var mTime = this.time % (60.0/this.Fspt);
 
-		if(mTime<this.Ti && this.Fspt > 0){
+		if(mTime<(2*this.Ti) && this.Fspt > 0){
 			return 0.5 * this.Pmax * (1 + Math.sin(
-						(2*Math.PI )* (mTime / this.Ti)- Math.PI/2
+						(2*Math.PI )* (mTime / (2*this.Ti))- Math.PI/2
 					));
 		}
 		else{ return 0; }
